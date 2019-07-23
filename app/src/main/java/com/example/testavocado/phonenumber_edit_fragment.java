0915c007b82892cfa7e034a92c86db4fc8625295
PoloGB <ahmadhamze.kawasaki@gmail.com>
2019-07-23 20:16:19 +0300

    ahmad update

--- app/src/main/java/com/example/testavocado/phonenumber_edit_fragment.java
+++ app/src/main/java/com/example/testavocado/phonenumber_edit_fragment.java
@@ -9,10 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.hbb20.CountryCodePicker;
 
 public class phonenumber_edit_fragment extends Fragment {
     public static final String TAG = "phonenumber_edit_fragme";
@@ -20,6 +23,7 @@ public class phonenumber_edit_fragment extends Fragment {
     private View myview;
     private TextView txtv_title;
     private ImageView imgv_arrow_back;
+    private CountryCodePicker countryCodePicker1;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -32,8 +36,18 @@ public class phonenumber_edit_fragment extends Fragment {
     {
         txtv_title=(TextView)myview.findViewById(R.id.txtv_post_created_firstname_merge_topbar_back_arrow);
         imgv_arrow_back=(ImageView)myview.findViewById(R.id.imgv_arrow_back_merge_topbar_back_arrow);
+        countryCodePicker1=(CountryCodePicker)myview.findViewById(R.id.coutrycodepicker1) ;
         //set on click
         imgv_arrow_back.setOnClickListener(new onclick());
+        countryCodePicker1.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                Toast.makeText(mcontext, "Updated " + countryCodePicker1.getSelectedCountryName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //set values
+        txtv_title.setText(getString(R.string.phonenumber_title)+"");
     }
 
 
